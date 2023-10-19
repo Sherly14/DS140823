4e1bf6bfec1d776c2530034fb090c02a7bf55ea6
Bharati Yadav <bharatiyadav14@gmail.com>
2023-10-19 21:31:50 +0530

    added register,login,module functionality

--- Python_Certification_Preparation/live_project_edyoda_replica/main.py
+++ Python_Certification_Preparation/live_project_edyoda_replica/main.py
@@ -1,71 +1,8 @@
-from operations import register
+from operations import register,login,add_module
 import random
 import re
 
-# class Main:
-
-#     def execute(self,choice):
-        
-#         if  choice == 1:
-#             print("******************Admin Login******************")
-#             ch='1: Login \n2: Register\n3: Exit'
-#             if ch==1:
-#                 username=input('Enter Username : ')
-#                 password=input('Enter Password : ')
-#             elif ch==2:
-#                 student_id=random.randint(1,10000)
-#                 name=input('Enter name : ')
-#                 mobile=int(input('Enter phone number  : '))
-#                 email=input('Enter email : ')
-#                 password=input('Enter password : ')
-#                 name_res=re.findall(r'^[a-zA-Z]+\\.?',name)
-#                 email_res = re.findall(r"^([A-z0-9]+[._])*[A-z0-9]+@[a-z]+(.[a-z]{2,})+$",email) 
-#                 mobile_res= re.findall(r'^[+91 ]{4}[6-9]{1}[0-9]{9}$',mobile)
-#                 password_res=re.findall(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$",password)
-#                 arr=[name_res,email_res,mobile_res,password_res]
-#                 if False not in arr :
-#                     pass
-#                 else:
-#                     for i in arr:
-#                         if i ==False:
-#                             print (f'Invalid {i} ')
-#                     self.execute(1)
-#             else:
-#                 exit(1)
-
-#         elif choice == 2:
-#             print("******************Student Login******************")
-#             ch='1: Login \n2: Register\n3: Exit'
-#             if ch==1:
-#                 username=input('Enter Username : ')
-#                 password=input('Enter Password : ')
-#             elif ch==2:
-#                 student_id=random.randint(1,10000)
-#                 name=input('Enter name : ')
-#                 mobile=int(input('Enter phone number  : '))
-#                 email=input('Enter email : ')
-#                 password=input('Enter password : ')
-#                 name_res=re.findall(r'^[a-zA-Z]+\\.?',name)
-#                 email_res = re.findall(r"^([A-z0-9]+[._])*[A-z0-9]+@[a-z]+(.[a-z]{2,})+$",email) 
-#                 mobile_res= re.findall(r'^[+91 ]{4}[6-9]{1}[0-9]{9}$',mobile)
-#                 password_res=re.findall(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$",password)
-#                 arr=[name_res,email_res,mobile_res,password_res]
-#                 if False not in arr :
-#                     pass
-#                 else:
-#                     for i in arr:
-#                         if i ==False:
-#                             print (f'Invalid {i} ')
-#                     self.execute(1)
-#             else:
-#                 exit(1)
-#         else:
-#             exit(1)
-        
-
 if __name__ == "__main__":
-    
-    # main = Main()
 
     while True:
         try:
@@ -85,6 +22,47 @@ if __name__ == "__main__":
                 print("****************Login as Student**************")
                 email_ID = input("Enter your email ID : ")
                 password = input("Enter your password : ")
+                login_flag = login("Student.json",email_ID,password)
+                print("Successfully Logged In!!!") if login_flag else print("Log In Failed!!!")
+
+            elif login_choice == 2:
+                print("****************Login as Admin**************")
+                email_ID = input("Enter your email ID : ")
+                password = input("Enter your password : ")
+                login_flag = login("Admin.json",email_ID,password)
+
+                if login_flag:
+                    while True:
+                        admin_choice = int(input("Enter \n1.Create Module \n2.View Module \n3.Update Module \n4.Delete Module \n5.Exit : \n"))
+
+                        if admin_choice == 1:
+                            module_ID = random.randint(1, 10000)
+                            module_name = input("Enter Module Name : ")
+                            start_date = input("Enter start date : ")
+                            end_date = input("Enter end date : ")
+                            
+                            size = int(input("Enter total topics you want to add : "))
+                            module_topic = []
+                            for i in range(size):
+                                topic = input("Enter topic :")
+                                module_topic.append(topic)
+
+                            module_flag = add_module("Module.json",module_ID,module_name,start_date,end_date,module_topic)
+                            print("Successfully Added!!!") if register_flag else print("Module Adding Failed!!!")
+
+                        elif admin_choice == 2:
+                            pass
+                        elif admin_choice == 3:
+                            pass
+                        elif admin_choice == 4:
+                            pass
+                        else:
+                            exit(1)
+
+                else: print("Log In Failed!!!")
+
+            else:
+                exit(1)
                 
 
         if choice == 2:
@@ -140,6 +118,10 @@ if __name__ == "__main__":
 
             else:
                 exit(1)
+
+
+        else:
+            exit(1)
                 
                 
 
@@ -150,5 +132,3 @@ if __name__ == "__main__":
         
         
         
-        
-        # main.execute(1)
--- Python_Certification_Preparation/live_project_edyoda_replica/operations.py
+++ Python_Certification_Preparation/live_project_edyoda_replica/operations.py
@@ -9,7 +9,7 @@ def register(filename,id,name,mobile,email,password):
         "Password" : password
     }
 
-    with open(filename,"r+") as file:
+    with open(filename,"w+") as file:
         try:
             data = json.load(file)
             if details not in data:
@@ -18,8 +18,54 @@ def register(filename,id,name,mobile,email,password):
                 file.truncate() # clear the file
                 json.dump(data,file)
                 return True
+        
+        except json.decoder.JSONDecodeError as ex:
+            lst = []
+            lst.append(details)
+            json.dump(lst,file)
+            return True
+        
         except Exception as ex:
             return False
 
-def login():
-    pass
+def login(filename,email_ID,password):
+    with open(filename,"r") as file: 
+        try:
+            data = json.load(file)
+            for i in data:
+                if i["Email"] == email_ID and i["Password"] == password:
+                    return True
+                else:
+                    return False
+        except Exception as ex:
+            print(ex)
+            return False
+
+          
+def add_module(filename,module_ID,module_name,start_date,end_date,module_topic):
+    details = {
+        "ID ": module_ID,
+        "Module Name ": module_name,
+        "Start Date ": start_date,
+        "End Date " : end_date,
+        "Module Topic" : module_topic
+    }
+
+    with open(filename,"w+") as file:
+        try:
+            data = json.load(file)
+            if details not in data:
+                data.append(details)
+                file.seek(0)
+                file.truncate() # clear the file
+                json.dump(data,file)
+                return True
+        
+        except json.decoder.JSONDecodeError as ex:
+            lst = []
+            lst.append(details)
+            json.dump(lst,file)
+            return True
+        
+        except Exception as ex:
+            return False
